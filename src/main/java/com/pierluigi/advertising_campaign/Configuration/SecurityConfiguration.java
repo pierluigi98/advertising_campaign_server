d8e0cd11cc2734f6da6ab2065cc79945ec28b64d
GiangiacomPi <pierluigi.giangiacomi@nttdata.com>
2020-10-14 15:07:04 +0200

    ok

--- src/main/java/com/pierluigi/advertising_campaign/Configuration/SecurityConfiguration.java
+++ src/main/java/com/pierluigi/advertising_campaign/Configuration/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.csrf().disable()
+                .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and().httpBasic();
     }
